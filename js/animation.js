7b55355d94e0f3808ccd2f3d10b9ff1ec60b6b99
Daria Khrushch <daria21khrushch@gmail.com>
2022-08-09 23:39:08 +0100

    adding video

--- js/animation.js
+++ js/animation.js
@@ -14,3 +14,14 @@ let elements = document.querySelectorAll('.element-animation');
 for (let elm of elements) {
   observer.observe(elm);
 }
+
+$(document).ready(function(){
+  
+  $('.open-modal').click(function(){
+    $('.modal-body iframe').attr('src', 'https://www.youtube.com/embed//F0p82zEIsL4');
+  });
+  
+  $("#myModal").on('hidden.bs.modal', function(){
+     $('.modal-body iframe').removeAttr('src');
+  });
+});
